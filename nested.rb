efe0f7ab2b0b5c49e50b1614419ae5a6609a7440
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-05 16:01:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,7 @@ def changing_alan
      alans_new_info = "GUI"
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      programmer_hash[:alan_kay][:known_for => alans_new_info]
-     return programmer_hash[:alan_kay][:known_for]
+     return programmer_hash
      
 end
 
@@ -128,7 +128,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
-     return programmer_hash[:dennis_ritchie][:languages][1]
-
+     programmer_hash
 end
