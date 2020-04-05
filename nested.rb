082e79157bdd45ca0ee353ed2ebaf967324edf1e
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-05 16:03:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,5 +128,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     return programmer_hash
+     programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+     return programmer_hash[:dennis_ritchie][:languages][1]
+
 end
