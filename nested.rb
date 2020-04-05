343bf2b988eb3726f7503a5837ae76ae904c8f2a
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-05 16:05:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,7 +107,7 @@ def changing_alan
      }
      alans_new_info = "GUI"
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     programmer_hash[:alan_kay][:known_for => alans_new_info]
+     programmer_hash[:alan_kay][:known_for] = alans_new_info]
      return programmer_hash
      
 end
